c21df8f0e8a997bb15d39820b70fa7d2abc12fb4
Matias <Matias@Mabla>
2013-11-10 14:03:44 -0300

    Autocomplete 14:03

---
+++ YoAmoMiBarrio/src/main/java/ar/com/yoamomibarrio/dao/PalabraClave.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.yoamomibarrio.dao;
+
+/**
+ *
+ * @author Matias
+ */
+public interface PalabraClave {
+    public Integer getId();
+    public String getDescripcion();
+}
